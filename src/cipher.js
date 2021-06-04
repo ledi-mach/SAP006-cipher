24a7b9ffa947c32c5bd97c14cf58d444b587df96
Lediane <lediane141@gmail.com>
2021-06-04 20:45:18 -0300

    Início JS e mudanças no HTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,16 @@
 const cipher = {
-  // ...
+  encode: function(key, txt){
+    let result = "";
+        for (let cont; cont <= txt.length; cont++) {
+            let codeLetter = txt.charCodeAt(cont); //para encontrar o código ASC de cada letra; 
+            if(codeLetter >= 65 && codeLetter <=90) {//codigo ASC vai de 65 a 90 (para maiúsculas);
+                let NewCode = (((codeLetter-65 + key) %26) + 65); //para pegar o código ASC já deslocado;
+                result += String.fromCharCode(newCode);
+            }         
+        }
+        return result;
+  }
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
+let txt = document.getElementById("field").value; //pegar o texto digitado
+let txtUpper= txt.toUpperCase(); //mudar o texto para caixa alta.
+let key = Number(document.getElementById("chave").value);//aqui pega o valor do deslocamento.
 
-console.log(cipher);
+document.getElementById("field").addEventListener.value = cipher();
+
+//Para chamar a função ao clicar:
+document.getElementById("field").addEventListener("click", cipher);
+
+
+/*
+}
+let key = Number(prompt("Digite a chave (somente números)")); //Pegar o valor da chave.
+if(key == 0 || key == "" ) {//aqui pesquisar como impedir a string;
+        alert("Favor digitar um número válido!");
+ } 
+    */
