affd9475de0dd9eff917f54367e9b80fda872286
Lediane <lediane141@gmail.com>
2021-06-06 19:52:34 -0300

    Continuando JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,30 @@
 const cipher = {
-  encode: function(key, txt){
-    let result = "";
-        for (let cont; cont <= txt.length; cont++) {
-            let codeLetter = txt.charCodeAt(cont); //para encontrar o código ASC de cada letra; 
-            if(codeLetter >= 65 && codeLetter <=90) {//codigo ASC vai de 65 a 90 (para maiúsculas);
-                let NewCode = (((codeLetter-65 + key) %26) + 65); //para pegar o código ASC já deslocado;
-                result += String.fromCharCode(newCode);
-            }         
-        }
-        return result;
-  }
-};
+  encode (cryptoffset, txt) {
+      let result = "";
+      for(let i = 0; i < txt.length; i++) {
+          let codeLetter = txt.charCodeAt(i); //para encontrar o código ASC de cada letra;
+          if(codeLetter >= 65 && codeLetter <= 90) {
+              let newCode = ((codeLetter - 65 + cryptoffset)%26) + 65; //para pegar o código ASC já deslocado;
+              result += String.fromCharCode(newCode);
+          } else {
+              result += String.fromCharCode(codeLetter);
+          }
+      }  return result;
+  },
+ // decode 
+  decode(cryptoffset, txt) {
+      let result = "";
+      for (let i = 0; i < txt.length ; i++) {
+          let codeLetter = txt.charCodeAt(i);
+          if (codeLetter >= 65 && codeLetter <= 90) {
+              let newCode = ((codeLetter - 65 - cryptoffset)%26) +65;
+              result += String.fromCharCode(newCode);
+          } else {
+              result += String.fromCharCode(codeLetter);
+          }
+      } return result;
+ }
 
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import cipher from './cipher.js';
-let txt = document.getElementById("field").value; //pegar o texto digitado
-let txtUpper= txt.toUpperCase(); //mudar o texto para caixa alta.
-let key = Number(document.getElementById("chave").value);//aqui pega o valor do deslocamento.
 
-document.getElementById("field").addEventListener.value = cipher();
-
-//Para chamar a função ao clicar:
-document.getElementById("field").addEventListener("click", cipher);
-
-
-/*
-}
-let key = Number(prompt("Digite a chave (somente números)")); //Pegar o valor da chave.
-if(key == 0 || key == "" ) {//aqui pesquisar como impedir a string;
-        alert("Favor digitar um número válido!");
- } 
-    */
+function valoresButton1() {
+        let txt = document.getElementById("field").value//pegar o texto digitado
+        let txtUpperCase = txt.toString().toUpperCase(); //Fiz errado: let txtUpperCase = string.toUpperCase(); 
+        let cryptoffset = Number(document.getElementById("chave").value);//aqui pega o valor do deslocamento.
+    
+        document.getElementById("field").value = cipher.encode(cryptoffset, txtUpperCase);
+    }
+    
+    function valoresButton2() {
+        let txt = document.getElementById("field").value//pegar o texto digitado
+        let txtUpperCase = txt.toString().toUpperCase(); //tava dando erro pq fiz: let txtUpperCase = string.toUpperCase(); 
+        let cryptoffset = Number(document.getElementById("chave").value);//aqui pega o valor do deslocamento.
+        document.getElementById("field").value = cipher.decode(cryptoffset, txtUpperCase);
+    }
+    
+    let buttonCoding1 = document.getElementById("coding");
+    buttonCoding1.addEventListener("click", valoresButton1);
+    let buttonCoding2 = document.getElementById("encoding");
+    buttonCoding2.addEventListener("click", valoresButton2);
+    
+    /*
+    let key = Number(prompt("Digite a chave (somente números)")); //Pegar o valor da chave.
+    if(key == 0 || key == "" ) {//aqui pesquisar como impedir a string;
+            alert("Favor digitar um número válido!");
+     } 
+        */
