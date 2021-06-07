0db1ed89c8b280006cb216032afcc2bcdca277d9
Lediane <lediane141@gmail.com>
2021-06-07 14:56:14 -0300

    Finalização JS e Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,31 @@
 const cipher = {
-  encode (cryptoffset, txt) {
-      let result = "";
-      for(let i = 0; i < txt.length; i++) {
-          let codeLetter = txt.charCodeAt(i); //para encontrar o código ASC de cada letra;
-          if(codeLetter >= 65 && codeLetter <= 90) {
-              let newCode = ((codeLetter - 65 + cryptoffset)%26) + 65; //para pegar o código ASC já deslocado;
-              result += String.fromCharCode(newCode);
-          } else {
-              result += String.fromCharCode(codeLetter);
-          }
-      }  return result;
-  },
- // decode 
-  decode(cryptoffset, txt) {
-      let result = "";
-      for (let i = 0; i < txt.length ; i++) {
-          let codeLetter = txt.charCodeAt(i);
-          if (codeLetter >= 65 && codeLetter <= 90) {
-              let newCode = ((codeLetter - 65 - cryptoffset)%26) +65;
-              result += String.fromCharCode(newCode);
-          } else {
-              result += String.fromCharCode(codeLetter);
-          }
-      } return result;
- }
+    encode (offset, txt) {
+        let result = " ";
+        for(let i = 0; i < txt.length; i++) {
+            let codeLetter = txt.charCodeAt(i); //para encontrar o código ASC de cada letra;
+            if(codeLetter >= 65 && codeLetter <= 90) {
+                let newCode = ((codeLetter - 65 + offset)%26) + 65; //para pegar o código ASC das maiúsculas já deslocado;
+                result += String.fromCharCode(newCode);
+            } else if (codeLetter >= 90 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
+                let newCode = ((codeLetter - 97 + offset)%26) +97;
+                result += String.fromCharCode(newCode);
+            } 
+        }  return result;
+    },
+   // decode 
 
+    decode(offset, txt) {
+        let result = " ";
+        for (let i = 0; i < txt.length ; i++) {
+            let codeLetter = txt.charCodeAt(i);
+            if (codeLetter >= 65 && codeLetter <= 90) {
+                let newCode = ((codeLetter - 65 - offset)%26) +65;
+                result += String.fromCharCode(newCode);
+            } else if (codeLetter >= 97 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
+                let newCode = ((codeLetter - 97 - offset)%26) +97;
+                result += String.fromCharCode(newCode);
+            }
+        }return result;
+    }
 }
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 import cipher from './cipher.js';
 
-function valoresButton1() {
-        let txt = document.getElementById("field").value//pegar o texto digitado
-        let txtUpperCase = txt.toString().toUpperCase(); //Fiz errado: let txtUpperCase = string.toUpperCase(); 
-        let cryptoffset = Number(document.getElementById("chave").value);//aqui pega o valor do deslocamento.
-    
-        document.getElementById("field").value = cipher.encode(cryptoffset, txtUpperCase);
-    }
-    
-    function valoresButton2() {
-        let txt = document.getElementById("field").value//pegar o texto digitado
-        let txtUpperCase = txt.toString().toUpperCase(); //tava dando erro pq fiz: let txtUpperCase = string.toUpperCase(); 
-        let cryptoffset = Number(document.getElementById("chave").value);//aqui pega o valor do deslocamento.
-        document.getElementById("field").value = cipher.decode(cryptoffset, txtUpperCase);
-    }
-    
-    let buttonCoding1 = document.getElementById("coding");
-    buttonCoding1.addEventListener("click", valoresButton1);
-    let buttonCoding2 = document.getElementById("encoding");
-    buttonCoding2.addEventListener("click", valoresButton2);
-    
-    /*
-    let key = Number(prompt("Digite a chave (somente números)")); //Pegar o valor da chave.
-    if(key == 0 || key == "" ) {//aqui pesquisar como impedir a string;
-            alert("Favor digitar um número válido!");
-     } 
-        */
+function valoresButton1 () {
+    let txt= document.getElementById("field").value;
+    let offset = Number(document.getElementById("chave").value);
+    document.getElementById("field").value = cipher.encode(offset, txt);
+}
+
+function valoresButton2() {
+    let txt = document.getElementById("field").value;
+    let offset = Number(document.getElementById("chave").value);
+    document.getElementById("field").value = cipher.decode(offset, txt);
+}
+
+let buttonCoding1 = document.getElementById("coding");
+buttonCoding1.addEventListener("click", valoresButton1);
+let buttonCoding2 = document.getElementById("encoding");
+buttonCoding2.addEventListener("click", valoresButton2);
