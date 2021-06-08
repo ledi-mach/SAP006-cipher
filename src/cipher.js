7256217056f1865bb389cd2a76dbc46709d04a9d
Lediane <lediane141@gmail.com>
2021-06-07 22:00:55 -0300

    Melhoria JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,14 @@
 const cipher = {
     encode (offset, txt) {
-        let result = " ";
+        //if()...aqui escrever a função para bloqueio se o parâmetro for incorreto.
+        let result = "";
         for(let i = 0; i < txt.length; i++) {
             let codeLetter = txt.charCodeAt(i); //para encontrar o código ASC de cada letra;
             if(codeLetter >= 65 && codeLetter <= 90) {
                 let newCode = ((codeLetter - 65 + offset)%26) + 65; //para pegar o código ASC das maiúsculas já deslocado;
                 result += String.fromCharCode(newCode);
             } else if (codeLetter >= 90 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
-                let newCode = ((codeLetter - 97 + offset)%26) +97;
+                let newCode = 122 - ((122 - codeLetter + offset) %26 );
                 result += String.fromCharCode(newCode);
             } 
         }  return result;
@@ -15,14 +16,14 @@ const cipher = {
    // decode 
 
     decode(offset, txt) {
-        let result = " ";
+        let result = "";
         for (let i = 0; i < txt.length ; i++) {
             let codeLetter = txt.charCodeAt(i);
             if (codeLetter >= 65 && codeLetter <= 90) {
-                let newCode = ((codeLetter - 65 - offset)%26) +65;
+                let newCode = ((codeLetter + 65 - offset)%26) +65;
                 result += String.fromCharCode(newCode);
             } else if (codeLetter >= 97 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
-                let newCode = ((codeLetter - 97 - offset)%26) +97;
+                let newCode = ((codeLetter - 97 - offset +26) %26) + 97;
                 result += String.fromCharCode(newCode);
             }
         }return result;
