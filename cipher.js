3c03402f1c71292dc85455d4699cf3485bed0d8f
Lediane <lediane141@gmail.com>
2021-06-18 12:52:49 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,16 +1,16 @@
 const cipher = {
-    encode (offset, txt) {
+    encode(offset, txt) {
         let result = "";
 
-        if((offset !== null && offset !== 0) && (txt !== "" && txt !== typeof"number")) {
-        for(let i = 0; i < txt.length; i++) {
-            let codeLetter = txt.charCodeAt(i); //para encontrar o código ASC de cada letra;
-            if(codeLetter >= 65 && codeLetter <= 90) {
-                let newCode = ((codeLetter - 65 + offset)%26) + 65; //para pegar o código ASC das maiúsculas já deslocado;
-                result += String.fromCharCode(newCode);
-            } else if(codeLetter >= 90 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
-                let newCode = ((codeLetter - 97 + offset)% 26) +97;
-                result += String.fromCharCode(newCode);
+        if ((offset !== null && offset !== 0) && (txt !== "" && txt !== typeof "number")) {
+            for (let i = 0; i < txt.length; i++) {
+                let codeLetter = txt.charCodeAt(i); //para encontrar o código ASC de cada letra;
+                if (codeLetter >= 65 && codeLetter <= 90) {
+                    let newCode = ((codeLetter - 65 + offset) % 26) + 65; //para pegar o código ASC das maiúsculas já deslocado;
+                    result += String.fromCharCode(newCode);
+                } else if (codeLetter >= 90 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
+                    let newCode = ((codeLetter - 97 + offset) % 26) + 97;
+                    result += String.fromCharCode(newCode);
                 } else {
                     result += String.fromCharCode(codeLetter);
                 }
@@ -23,21 +23,21 @@ const cipher = {
     decode(offset, txt) {
         let result = "";
 
-        if((offset !== null && offset !== 0) && (txt !== "" && txt !== typeof"number")) {
-        for (let i = 0; i < txt.length ; i++) {
-            let codeLetter = txt.charCodeAt(i);
-            if(codeLetter >= 65 && codeLetter <= 90) {
-                let newCode = ((codeLetter + 65 - offset)%26) +65;
-                result += String.fromCharCode(newCode);
-            } else if (codeLetter >= 97 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
-                let newCode = 122 -((122 - codeLetter + offset)%26);
-                result += String.fromCharCode(newCode);
-            } else {
-                result += String.fromCharCode(codeLetter);
-            }
-        } return result;
-    } else {
-        throw TypeError('Dados inválidos');
+        if ((offset !== null && offset !== 0) && (txt !== "" && txt !== typeof "number")) {
+            for (let i = 0; i < txt.length; i++) {
+                let codeLetter = txt.charCodeAt(i);
+                if (codeLetter >= 65 && codeLetter <= 90) {
+                    let newCode = ((codeLetter + 65 - offset) % 26) + 65;
+                    result += String.fromCharCode(newCode);
+                } else if (codeLetter >= 97 && codeLetter <= 122) {//para pegar o código ASC das minúsculas já deslocado;
+                    let newCode = 122 - ((122 - codeLetter + offset) % 26);
+                    result += String.fromCharCode(newCode);
+                } else {
+                    result += String.fromCharCode(codeLetter);
+                }
+            } return result;
+        } else {
+            throw TypeError('Dados inválidos');
         }
     }
 }
